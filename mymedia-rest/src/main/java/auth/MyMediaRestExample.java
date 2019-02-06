cba2964588c1788dde1542d9194f054fe998fb54
Kiss Dávid <kiss.david@matrix2.meditcom.hu>
2019-02-06 15:28:01 +0100

    configure web xml

--- mymedia-rest/src/main/java/auth/MyMediaRestExample.java
+++ mymedia-rest/src/main/java/auth/MyMediaRestExample.java
@@ -1,9 +1,6 @@
 package auth;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 
@@ -14,6 +11,7 @@ public class MyMediaRestExample extends Application {
 
     @GET
     @Path(("/sayhello"))
+    //@Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public String sayHello(){
         return "hello";
