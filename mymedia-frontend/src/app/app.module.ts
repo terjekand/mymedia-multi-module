dc0190af6d88dec3813b08e4ba17156aa109c91b
Kiss David <kissdavid0506@gmail.com>
2019-04-06 14:59:03 +0200

    start the login and the newsfeed page

--- mymedia-frontend/src/app/app.module.ts
+++ mymedia-frontend/src/app/app.module.ts
@@ -3,14 +3,46 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+
+const appRouter: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'newsfeed',
+    component: NewsFeedComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+  {
+    path: '',
+    component: LoginComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LoginComponent,
+    NewsFeedComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRouter, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
